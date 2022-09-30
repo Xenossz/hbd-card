4f61b2587e5fb4130c2dc76a8f3f9a7f71132f30
vivekkhimani <vivekkhimani07@gmail.com>
2022-09-30 05:24:28 -0400

    remote override

--- builder/init.js
+++ builder/init.js
@@ -8,13 +8,13 @@ const {
   generateMarkupRemote,
 } = require("./generateMarkup");
 
-require("dotenv").config();
+// require("dotenv").config();
 
 // if (!process.env.NAME) throw new Error("Please specify NAME in environment.");
 // if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
 
-const picPath = process.env.PIC;
-const msgPath = process.env.SCROLL_MSG;
+// const picPath = process.env.PIC;
+// const msgPath = process.env.SCROLL_MSG;
 
 //Local initialization
 const setLocalData = async () => {
